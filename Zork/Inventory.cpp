63f4ac7ec6614811d105150ea5244441e12fe1e6
Anthony Bartholomew <abar15.abb@gmail.com>
2019-04-17 20:55:32 -0400

    Added comments to inventory

--- Zork/Inventory.cpp
+++ Zork/Inventory.cpp
@@ -12,12 +12,14 @@ Inventory::~Inventory()
 
 void Inventory::displayInventory()
 {
+	// Check if there are no items
 	if (this->inventory.size() == 0)
 	{
 		std::cout << "There are no items in your inventory!" << std::endl;
 	}
 	else
 	{
+		// Output a each element that is in the inventory
 		std::cout << "Current Inventory: " << std::endl;
 		for (auto pair : this->inventory)
 		{
@@ -28,19 +30,23 @@ void Inventory::displayInventory()
 
 void Inventory::addToInventory(std::string itemName)
 {
+	// Check if item is not already in the inventory
 	if (this->inventory.find(itemName) == this->inventory.end())
 	{
 		this->inventory.insert(std::make_pair(itemName, 1));
 	}
 	else
 	{
+		// Get a pointer to the pair in the inventory
 		auto pair = this->inventory.find(itemName);
+		// Increment the amount of the particular item
 		pair->second++;
 	}
 }
 
 void Inventory::removeInventory(std::string itemName)
 {
+	// Prevent code from running when the item being removed does not exist
 	assert(this->inventory.find(itemName) != this->inventory.end());
 
 	auto pair = this->inventory.find(itemName);
--- Zork/Inventory.h
+++ Zork/Inventory.h
@@ -13,9 +13,35 @@ public:
 	Inventory();
 	~Inventory();
 
+    /**
+	 * Outputs the all items currently in the inventory
+	 */
 	void displayInventory();
+
+	/**
+	 * Adds an item to the inventory
+	 *
+	 * @param itemName
+	 *		A string of the item name that will be added to the inventory
+	 */
 	void addToInventory(std::string itemName);
+
+	/**
+	 * Removes an item from the inventory
+	 *
+	 * @param itemName
+	 *		A string of the item name that will be removed the the invetory
+	 */
 	void removeInventory(std::string itemName);
+
+	/**
+	 * Checks if the item is in the inventory
+	 *
+	 * @param itemName
+	 *		A string of the item name that is checked if it is in the inventory
+	 * @return
+	 *		A boolean value that indicates if the item is in the inventory
+	 */
 	bool hasItem(std::string itemName);
 };
 
